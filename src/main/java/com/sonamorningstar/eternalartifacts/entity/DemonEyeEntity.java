a7baf0a5b506c0f86ae6c54d36478f75b877881e
Aziz DAĞLI <45048634+KodYazanDruid@users.noreply.github.com>
2023-05-23 20:32:21 +0300

    Demon Eye AI

--- src/main/java/com/sonamorningstar/eternalartifacts/entity/DemonEyeEntity.java
+++ src/main/java/com/sonamorningstar/eternalartifacts/entity/DemonEyeEntity.java
@@ -1,18 +1,21 @@
 package com.sonamorningstar.eternalartifacts.entity;
 
 import net.minecraft.util.Mth;
+import net.minecraft.world.Difficulty;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.FlyingMob;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.ai.control.BodyRotationControl;
+import net.minecraft.world.entity.ai.control.LookControl;
 import net.minecraft.world.entity.ai.control.MoveControl;
 import net.minecraft.world.entity.ai.goal.*;
 import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
 import net.minecraft.world.entity.monster.Enemy;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.pathfinder.BlockPathTypes;
 import net.minecraft.world.phys.Vec3;
 import software.bernie.geckolib3.core.IAnimatable;
 import software.bernie.geckolib3.core.PlayState;
@@ -28,9 +31,12 @@ import java.util.Random;
 
 public class DemonEyeEntity extends FlyingMob implements Enemy, IAnimatable {
     private AnimationFactory factory = GeckoLibUtil.createFactory(this);
+    Vec3 moveTargetPoint = Vec3.ZERO;
 
     public DemonEyeEntity(EntityType<? extends DemonEyeEntity> pEntityType, Level pLevel) {
         super(pEntityType, pLevel);
+        this.moveControl = new DemonEyeEntity.DemonEyeMoveControl(this);
+        this.lookControl = new DemonEyeEntity.DemonEyeLookControl(this);
 
     }
 
@@ -43,16 +49,31 @@ public class DemonEyeEntity extends FlyingMob implements Enemy, IAnimatable {
     }
 
     @Override
+    protected BodyRotationControl createBodyControl() { return new DemonEyeEntity.DemonEyeBodyRotationControl(this); }
+
+    @Override
     protected void registerGoals() {
         this.goalSelector.addGoal(4, new AIAttack(this));
         this.goalSelector.addGoal(5, new AIFlyTowardsTarget(this));
         this.goalSelector.addGoal(6, new AIRandomFly(this));
-        this.goalSelector.addGoal(7, new AILookAround(this));
-        this.targetSelector.addGoal(3, new NearestAttackableTargetGoal<>(this, Player.class, true));
+        this.goalSelector.addGoal(7, new RandomLookAroundGoal(this));
+        this.targetSelector.addGoal(1, new NearestAttackableTargetGoal<>(this, Player.class, true));
+
+        /*
+        this.goalSelector.addGoal(5, new LookAtPlayerGoal(this, Player.class, 20F));
+         */
+    }
+
+    private void despawnIfPeaceful() {
+        if (!level.isClientSide && level.getDifficulty() == Difficulty.PEACEFUL)
+            discard();
     }
 
     @Override
-    public void aiStep() { super.aiStep(); }
+    public void aiStep() {
+        super.aiStep();
+        despawnIfPeaceful();
+    }
 
     private <E extends IAnimatable> PlayState predicate(AnimationEvent<E> event) {
         if (event.getController().getCurrentAnimation() == null) {
@@ -70,6 +91,55 @@ public class DemonEyeEntity extends FlyingMob implements Enemy, IAnimatable {
     @Override
     public AnimationFactory getFactory() { return this.factory; }
 
+    class DemonEyeLookControl extends LookControl {
+        public DemonEyeLookControl(Mob pMob) { super(pMob); }
+        public void tick() { }
+    }
+
+    class DemonEyeMoveControl extends MoveControl {
+        public DemonEyeMoveControl(Mob pMob) {super(pMob); }
+        private float speed = 0.1F;
+
+        public void tick() {
+            if (DemonEyeEntity.this.horizontalCollision) {
+                DemonEyeEntity.this.setYRot(DemonEyeEntity.this.getYRot() + 180.0F);
+                this.speed = 0.1F;
+            }
+
+            double d0 = DemonEyeEntity.this.moveTargetPoint.x - DemonEyeEntity.this.getX();
+            double d1 = DemonEyeEntity.this.moveTargetPoint.y - DemonEyeEntity.this.getY();
+            double d2 = DemonEyeEntity.this.moveTargetPoint.z - DemonEyeEntity.this.getZ();
+            double d3 = Math.sqrt(d0 * d0 + d2 * d2);
+            if (Math.abs(d3) > (double)1.0E-5F) {
+                double d4 = 1.0D - Math.abs(d1 * (double)0.7F) / d3;
+                d0 *= d4;
+                d2 *= d4;
+                d3 = Math.sqrt(d0 * d0 + d2 * d2);
+                double d5 = Math.sqrt(d0 * d0 + d2 * d2 + d1 * d1);
+                float f = DemonEyeEntity.this.getYRot();
+                float f1 = (float)Mth.atan2(d2, d0);
+                float f2 = Mth.wrapDegrees(DemonEyeEntity.this.getYRot() + 90.0F);
+                float f3 = Mth.wrapDegrees(f1 * (180F / (float)Math.PI));
+                DemonEyeEntity.this.setYRot(Mth.approachDegrees(f2, f3, 4.0F) - 90.0F);
+                DemonEyeEntity.this.yBodyRot = DemonEyeEntity.this.getYRot();
+                if (Mth.degreesDifferenceAbs(f, DemonEyeEntity.this.getYRot()) < 3.0F) {
+                    this.speed = Mth.approach(this.speed, 1.8F, 0.005F * (1.8F / this.speed));
+                } else {
+                    this.speed = Mth.approach(this.speed, 0.2F, 0.025F);
+                }
+
+                float f4 = (float)(-(Mth.atan2(-d1, d3) * (double)(180F / (float)Math.PI)));
+                DemonEyeEntity.this.setXRot(f4);
+                float f5 = DemonEyeEntity.this.getYRot() + 90.0F;
+                double d6 = (double)(this.speed * Mth.cos(f5 * ((float)Math.PI / 180F))) * Math.abs(d0 / d5);
+                double d7 = (double)(this.speed * Mth.sin(f5 * ((float)Math.PI / 180F))) * Math.abs(d2 / d5);
+                double d8 = (double)(this.speed * Mth.sin(f4 * ((float)Math.PI / 180F))) * Math.abs(d1 / d5);
+                Vec3 vec3 = DemonEyeEntity.this.getDeltaMovement();
+                DemonEyeEntity.this.setDeltaMovement(vec3.add((new Vec3(d6, d8, d7)).subtract(vec3).scale(0.2D)));
+            }
+
+        }
+    }
 
     // AI
     static class AIFlyTowardsTarget extends Goal {
@@ -118,30 +188,22 @@ public class DemonEyeEntity extends FlyingMob implements Enemy, IAnimatable {
 
         @Override
         public void start() {
-            if (taskOwner.getTarget() != null)
-                taskOwner.doHurtTarget(taskOwner.getTarget());
+            if (taskOwner.getTarget() != null) { taskOwner.doHurtTarget(taskOwner.getTarget()); }
             attackTick = 20;
         }
     }
 
     static class AIRandomFly extends Goal {
-        private final DemonEyeEntity parentEntity;
+        private final DemonEyeEntity parent;
 
-        public AIRandomFly(DemonEyeEntity demonEye) {
-            this.parentEntity = demonEye;
-            this.setFlags(EnumSet.of(Flag.MOVE));
+        public AIRandomFly(DemonEyeEntity mob) {
+            this.parent = mob;
+            this.setFlags(EnumSet.of(Goal.Flag.MOVE));
         }
 
         @Override
         public boolean canUse() {
-            MoveControl moveControl = this.parentEntity.getMoveControl();
-            if (!moveControl.hasWanted()) { return true; } else {
-                double d0 = moveControl.getWantedX() - this.parentEntity.getX();
-                double d1 = moveControl.getWantedY() - this.parentEntity.getY();
-                double d2 = moveControl.getWantedZ() - this.parentEntity.getZ();
-                double d3 = d0 * d0 + d1 * d1 + d2 * d2;
-                return d3 < 1.0 || d3 > 3600.0;
-            }
+            return !this.parent.getMoveControl().hasWanted() && this.parent.getRandom().nextFloat() < 0.01F;
         }
 
         @Override
@@ -149,40 +211,55 @@ public class DemonEyeEntity extends FlyingMob implements Enemy, IAnimatable {
 
         @Override
         public void start() {
-            Random random = this.parentEntity.getRandom();
-            double d0 = this.parentEntity.getX() + (random.nextFloat() - 0.5F) * 16.0F;
-            double d1 = this.parentEntity.getY() + (random.nextFloat() - 0.5F) * 16.0F;
-            double d2 = this.parentEntity.getZ() + (random.nextFloat() - 0.5F) * 16.0F;
-            this.parentEntity.getMoveControl().setWantedPosition(d0, d1, d2, 1.0);
+            Random random = this.parent.getRandom();
+            double d0 = this.parent.getX() + (random.nextFloat() * 2.0F - 1.0F) * 3.0F;
+            double d1 = this.parent.getY() + (random.nextFloat() * 2.0F - 1.0F) * 3.0F;
+            double d2 = this.parent.getZ() + (random.nextFloat() * 2.0F - 1.0F) * 3.0F;
+            this.parent.getMoveControl().setWantedPosition(d0, d1, d2, 0.15D);
         }
     }
 
     public static class AILookAround extends Goal {
-        private final DemonEyeEntity parentEntity;
+        private final DemonEyeEntity parent;
+        private double lookX;
+        private double lookZ;
+        private int idleTime;
 
-        public AILookAround(DemonEyeEntity demonEye) {
-            this.parentEntity = demonEye;
-            this.setFlags(EnumSet.of(Flag.LOOK));
+        public AILookAround(DemonEyeEntity mob) {
+            this.parent = mob;
+            this.setFlags(EnumSet.of(Goal.Flag.LOOK));
         }
 
         @Override
-        public boolean canUse() { return true; }
+        public boolean canUse() { return this.parent.getRandom().nextFloat() < 0.008F; }
+
+        @Override
+        public boolean canContinueToUse() { return this.idleTime >= 0; }
+
+        @Override
+        public void start() {
+            double d0 = (Math.PI * 2.0D) * this.parent.getRandom().nextDouble();
+            this.lookX = Math.cos(d0);
+            this.lookZ = Math.sin(d0);
+            this.idleTime = 20 + this.parent.getRandom().nextInt(20);
+        }
 
         @Override
         public void tick() {
-            if (this.parentEntity.getTarget() == null) {
-                this.parentEntity.setYRot(-((float) Mth.atan2(this.parentEntity.getDeltaMovement().x(), this.parentEntity.getDeltaMovement().z())) * (180F / (float) Math.PI));
-                this.parentEntity.setYBodyRot(this.parentEntity.getYRot());
-            } else {
-                LivingEntity entitylivingbase = this.parentEntity.getTarget();
+            --this.idleTime;
+            this.parent.setYRot(-((float)Mth.atan2(this.lookX, this.lookZ)) * (180.0F / (float)Math.PI));
+            this.parent.yBodyRot = this.parent.getYRot();
+        }
+    }
 
-                if (entitylivingbase.distanceToSqr(this.parentEntity) < 4096.0D) {
-                    double d1 = entitylivingbase.getX() - this.parentEntity.getX();
-                    double d2 = entitylivingbase.getZ() - this.parentEntity.getZ();
-                    this.parentEntity.setYRot(-((float) Mth.atan2(d1, d2)) * (180F / (float) Math.PI));
-                    this.parentEntity.setYBodyRot(this.parentEntity.getYRot());
-                }
-            }
+    class DemonEyeBodyRotationControl extends BodyRotationControl {
+        public DemonEyeBodyRotationControl(Mob pMob) {
+            super(pMob);
+        }
+
+        public void clientTick() {
+            DemonEyeEntity.this.yHeadRot = DemonEyeEntity.this.yBodyRot;
+            DemonEyeEntity.this.yBodyRot = DemonEyeEntity.this.getYRot();
         }
     }
 }
